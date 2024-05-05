4f9e8e5e4d0ebeffda8e69eb0ee1146cad60b93d
VivekMara <vivek.v.mara@gmail.com>
2024-05-05 20:25:57 +0530

    updated styles

--- src/script.js
+++ src/script.js
@@ -10,19 +10,19 @@ function add_task() {
     const new_task = document.createElement("li");
     new_task.textContent = task.value;
     display.appendChild(new_task);
+    task.value = "";
+    const remove_btn = document.createElement("button");
+    remove_btn.textContent = "Mark Completed";
+    remove_btn.style.margin = "10px";
+    remove_btn.style.border = "dotted";
+    remove_btn.style.borderRadius = "10px";
+    remove_btn.style.padding = "5px";
+    new_task.appendChild(remove_btn);
+    remove_btn.id = "remove_btn";
+    remove_btn.onclick = remove_task;
+    count = count + 1;
+    counter.textContent = count;
   }
-
-  const remove_btn = document.createElement("button");
-  remove_btn.textContent = "Mark Completed";
-  remove_btn.style.margin = "10px";
-  remove_btn.style.border = "dotted";
-  remove_btn.style.borderRadius = "10px";
-  remove_btn.style.padding = "5px";
-  new_task.appendChild(remove_btn);
-  remove_btn.id = "remove_btn";
-  remove_btn.onclick = remove_task;
-  count = count + 1;
-  counter.textContent = count;
 }
 
 function remove_task() {
