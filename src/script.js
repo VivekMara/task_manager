1c4508b937c4be1223b270ea229bf174a54fd8c6
VivekMara <vivek.v.mara@gmail.com>
2024-05-05 19:04:27 +0530

    added the functionality to remove the task as well as count the number of tasks.

--- src/script.js
+++ src/script.js
@@ -1,20 +1,31 @@
 const task = document.getElementById("input_task");
-
-
+let count = 0;
+const counter = document.getElementById("counter");
+const display = document.querySelector(".task_display");
 
 
 
 function add_task(){
-    const new_task = document.createElement("h1");   
+    const new_task = document.createElement("li");
     new_task.textContent = task.value;
-    const display = document.querySelector(".task_display");
     display.appendChild(new_task);
-
-
+    const remove_btn = document.createElement("button");
+    remove_btn.textContent= "Mark Completed";
+    remove_btn.style.margin = "10px";
+    remove_btn.style.border = "dotted";
+    remove_btn.style.borderRadius = "10px";
+    remove_btn.style.padding = "5px"
+    new_task.appendChild(remove_btn);
+    remove_btn.id = "remove_btn";
+    remove_btn.onclick = remove_task;
+    count = count + 1;
+    counter.textContent = count; 
     
-
 }
 
 function remove_task(){
-
+    this.parentNode.remove();
+    count = count - 1;
+    counter.textContent = count;
 }
+
