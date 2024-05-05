113a36202825391f5bfafbcd7e669276c1940d70
VivekMara <vivek.v.mara@gmail.com>
2024-05-05 20:16:50 +0530

    added alert for empty input field

--- src/script.js
+++ src/script.js
@@ -3,29 +3,30 @@ let count = 0;
 const counter = document.getElementById("counter");
 const display = document.querySelector(".task_display");
 
-
-
-function add_task(){
+function add_task() {
+  if (task.value === "") {
+    alert("Enter a Task!!");
+  } else {
     const new_task = document.createElement("li");
     new_task.textContent = task.value;
     display.appendChild(new_task);
-    const remove_btn = document.createElement("button");
-    remove_btn.textContent= "Mark Completed";
-    remove_btn.style.margin = "10px";
-    remove_btn.style.border = "dotted";
-    remove_btn.style.borderRadius = "10px";
-    remove_btn.style.padding = "5px"
-    new_task.appendChild(remove_btn);
-    remove_btn.id = "remove_btn";
-    remove_btn.onclick = remove_task;
-    count = count + 1;
-    counter.textContent = count; 
-    
+  }
+
+  const remove_btn = document.createElement("button");
+  remove_btn.textContent = "Mark Completed";
+  remove_btn.style.margin = "10px";
+  remove_btn.style.border = "dotted";
+  remove_btn.style.borderRadius = "10px";
+  remove_btn.style.padding = "5px";
+  new_task.appendChild(remove_btn);
+  remove_btn.id = "remove_btn";
+  remove_btn.onclick = remove_task;
+  count = count + 1;
+  counter.textContent = count;
 }
 
-function remove_task(){
-    this.parentNode.remove();
-    count = count - 1;
-    counter.textContent = count;
+function remove_task() {
+  this.parentNode.remove();
+  count = count - 1;
+  counter.textContent = count;
 }
-
